38b8bb427650410e6995efa5d0d79b95a4e1c1b7
Jordan Nelson <nelsonjordan20@yahoo.com>
2015-03-31 11:08:05 -0600

    Initial Commit

---
+++ ActivityViewController/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ActivityViewController
+//
+//  Created by Jordan Nelson on 3/31/15.
+//  Copyright (c) 2015 Jordan Nelson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ActivityViewController/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ActivityViewController
+//
+//  Created by Jordan Nelson on 3/31/15.
+//  Copyright (c) 2015 Jordan Nelson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
